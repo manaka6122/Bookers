70a05a1a64b6c47887b60c1a1c74d5d1b97b0dee
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-02-08 11:52:41 +0000

    [Finish] 3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def new
-    @book = List.new
+    @book = Book.new
   end
 
   def index
@@ -9,23 +9,36 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to show_books_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
+   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+   end
+
   def edit
     @book = Book.find(params[:id])
   end
 
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-  get 'books' => 'books#index', as: 'index_books'
-  post 'books' => 'books#create', as: 'create_books'
-  get 'books/:id' => 'books#show', as: 'show_books'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_books'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
