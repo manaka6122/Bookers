b4fd0a558dfbab55adbb42aa958d189a00bfa593
EC2 Default User <ec2-user@ip-172-31-17-86.ec2.internal>
2022-02-08 02:57:54 +0000

    [Add] 投稿詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def new
+    @book = List.new
   end
 
   def index
@@ -10,7 +11,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to show_books_path(book)
+    redirect_to show_books_path(book.id)
   end
 
   def show
@@ -19,6 +20,12 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   post 'books' => 'books#create', as: 'create_books'
   get 'books/:id' => 'books#show', as: 'show_books'
   get 'books/:id/edit' => 'books#edit', as: 'edit_books'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
